4e4bb9513054e4916b8d82b553c30f0d6377b042
Arno de Jong <arno@elephantcs.nl>
2019-09-24 10:16:11 +0200

    Adds WIP changes to the driver controller and view

--- api/controllers/DriverController.js
+++ api/controllers/DriverController.js
@@ -2,10 +2,12 @@ const fetch = require("node-fetch");
 
 //TODO: Add stuff
 module.exports = {
-  view: async function() {
+  view: async function(req, res) {
     const response = await fetch('http://ergast.com/api/f1/2019/drivers.json')
     const myJson = await response.json();
-    const driverList = myJson.MRData.DriverTable.Drivers;
+    const drivers = myJson.MRData.DriverTable.Drivers;
+    const string = JSON.stringify(drivers);
+    return res.view('pages/drivers', {drivers});
     driverList.forEach(driver => {
       console.log(driver.givenName + driver.familyName);
     });
--- config/routes.js
+++ config/routes.js
@@ -20,7 +20,7 @@ module.exports.routes = {
   'GET /legal/terms':        { action:   'legal/view-terms' },
   'GET /legal/privacy':      { action:   'legal/view-privacy' },
   'GET /contact':            { action:   'view-contact' },
-  'GET /drivers':            { action:   'view-drivers' },
+  'GET /drivers':            [{ action:   'view-drivers' }, 'Driver.view'],
 
   'GET /signup':             { action: 'entrance/view-signup' },
   'GET /email/confirm':      { action: 'entrance/confirm-email' },
