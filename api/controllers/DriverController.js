98e1a8b637ab4c8de2448ce4d4351a070b31570a
Arno de Jong <arno@elephantcs.nl>
2019-09-30 11:28:49 +0200

    Improves filtering functionality

--- api/controllers/DriverController.js
+++ api/controllers/DriverController.js
@@ -5,20 +5,20 @@ module.exports = {
   view: async function(req, res) {
     const response = await fetch('http://ergast.com/api/f1/2019/drivers.json')
     const myJson = await response.json();
-    const drivers = myJson.MRData.DriverTable.Drivers;
-    const query = req.query.name;
-    if(query) {
+    let drivers = myJson.MRData.DriverTable.Drivers;
+    if (req.query.name) {
+      const query = req.query.name.toLowerCase();
+      newDrivers = [];
       drivers.forEach(function(driver) {
-        if (driver.givenName == query) {
-          let drivers = [];
-          drivers.push(driver);
-          return res.view('pages/drivers', {drivers});
-        } else {
+        const fullName = (driver.givenName + ' ' + driver.familyName).toLowerCase();
+        if (fullName.indexOf(query) > -1) {
+          newDrivers.push(driver);
         }
-      })
-    } else {
-      return res.view('pages/drivers', {drivers});
-    };
+      });
+      drivers = newDrivers;
+    }
+    return res.view('pages/drivers', {drivers});
+
   }
 
 };
